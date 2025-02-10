99a1c6d4daca9a4d4cbd6e10c800162e0263e13f
Pranav-cse-kalvium <pranav.prakash@kalvium.community>
2025-02-10 14:17:25 +0530

    branch landing

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="container">
+            <h1>Welcome to my Lalaland Asap Project </h1>
+            <p className='lalalund'>This is the landing page for my lalaland ASAP project.</p>
+        </div>
+    );
+};
+
+export default App;
