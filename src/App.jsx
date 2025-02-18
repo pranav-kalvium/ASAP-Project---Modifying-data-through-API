bce128046a76fa82eb27416f52bebf71bc8802a8
pranav-kalvium <pranavprakash6410@gmail.com>
2025-02-18 14:04:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import './App.css';
 const App = () => {
     return (
         <div className="container">
-            <h1>Welcome to my Lalaland Asap Project </h1>
-            <p className='lalalund'>This is the landing page for my lalaland ASAP project.</p>
+            <h1>Welcome to my  Asap Project </h1>
+            <p className='lalalund'>This is the landing page for my ASAP project.</p>
         </div>
     );
 };
